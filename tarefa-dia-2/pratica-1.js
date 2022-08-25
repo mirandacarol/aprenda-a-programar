d72187e1811f2a3945ac130f354c0d92f83f872f
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-25 00:37:54 -0300

    Console log e desafio opcional

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -18,3 +18,4 @@ let hobbies = "Praticar artes marciais e ler livros variados"
 console.log(hobbies)
 let frasedeefeito = "Eu não sou melhor que ninguem, mas ninguem é melhor do que eu"
 console.log(frasedeefeito)
+console.log(`Olá ${nome}! Somos aqui da agência de turismo Viagem dos sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${localquesonhaemconhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
