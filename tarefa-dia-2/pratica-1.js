fb804e04f6b68c85d6ce30e052b6c0f57420273d
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-25 16:20:47 -0300

    Console log,desafio opcional e praticando operador matem.

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -19,3 +19,16 @@ console.log(hobbies)
 let frasedeefeito = "Eu não sou melhor que ninguem, mas ninguem é melhor do que eu"
 console.log(frasedeefeito)
 console.log(`Olá ${nome}! Somos aqui da agência de turismo Viagem dos sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${localquesonhaemconhecer}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
+//praticando operadores matemáticos
+let numero1 = 500
+let numero2 = 200
+console.log(numero1 + numero2)
+console.log(numero1 / numero2)
+let numero3 = 5
+let numero4 = 20
+console.log(numero3 * numero4)
+let numero5 = 100
+let numero6 = 50
+console.log(numero5 - numero6)
+
+
