7cf010a016d75b4bc90065b859a79de6c75f06e1
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-24 23:58:10 -0300

    Comandos de log

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,8 +5,16 @@ let profissao
 let idade
 let localquesonhaemconhecer
 nome = "Carolina"
-email = "carolinamiranda.adm@gmail.carolinamiranda"
+email = "carolinamiranda.adm@gmail.com"
 profissao = "Administradora"
 idade = "37"
 localquesonhaemconhecer = "Tailandia"
-
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(localquesonhaemconhecer)
+let hobbies = "Praticar artes marciais e ler livros variados"
+console.log(hobbies)
+let frasedeefeito = "Eu não sou melhor que ninguem, mas ninguem é melhor do que eu"
+console.log(frasedeefeito)
