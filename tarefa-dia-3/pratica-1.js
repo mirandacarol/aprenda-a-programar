8e146399f425a259aece46d13efc0c106243f25c
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-27 14:20:12 -0300

    exibindo números de 1 a 12

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,10 @@
+//exibir números de 1 até n
+//Considere que o dado de entrada para executar o código é o número n. Ou seja, uma variável n que vai armazenar o valor do número final que queremos exibir
+//Neste problema, você deverá utilizar o comando while
+
+let i = 1
+
+while(i <= 12){
+    console.log(i)
+    i = i + 1
+}
