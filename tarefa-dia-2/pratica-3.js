c63d7046f509b44190d487a53dc644bde6a541ee
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-27 12:43:43 -0300

    conclusão pratica3

---
+++ tarefa-dia-2/pratica-3.js
@@ -0,0 +1,9 @@
+//O matemático e cientista da computação Alan Turing estava decifrando um dos códigos da máquina de criptografia Enigma durante a 2ª guerra mundial. Ele descobriu que se o código tiver mais que 6110 caracteres, a mensagem codificada não poderia ser decifrada em tempo hábil para derrotar os nazistas. Por isso ele pediu a ajuda dos programadores do ITuring para ajudá-lo a contabilizar quantos caracteres tem essa mensagem e assim dizer para ele se ('if') eles terão tempo hábil ou não para derrotar os nazistas
+
+let mensagemCodificada = 6110
+
+if (mensagemCodificada > 6110) {
+    console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'(")
+} else {
+console.log("Maravilha! O código tem menos ou é igual a 6110 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D")
+}
