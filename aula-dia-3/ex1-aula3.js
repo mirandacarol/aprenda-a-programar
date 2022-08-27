1d8eab4744a4823f570e6ed576e1933f489e4b4e
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-26 21:44:21 -0300

    if e else

---
+++ aula-dia-3/ex1-aula3.js
@@ -0,0 +1,10 @@
+/* dado um valor, me diga apenas se ele é do tipo number */
+
+let valor = 12 // string
+// para saber se é uma string comando typeof
+
+
+if (typeof(valor) === "number") {
+
+console.log(`${valor} é do tipo ${typeof(valor)}`)
+}
---
+++ aula-dia-3/ex2-aula3.js
@@ -0,0 +1,12 @@
+/*SESSAO 1: DOMINANDO LOOPS E CONTADORES
+
+Exercicio 1: exibindo números de 1 a 10
+*/
+
+let contador = 1
+
+while (contador <=10) {
+    console.log(contador)
+    contador = contador + 1
+}
+
