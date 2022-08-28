04c957ef5c6e0e1ada1de5ceb31da01d8e92826e
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-27 22:24:28 -0300

    Exiba os ímpares

---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,10 @@
+//Crie um código no arquivo pratica-3.js que começando com um número inteiro > 0, exiba com console.log() todos os números ímpares até chegar neste número. Se quiser, pode chamar a variável que vai armazenar esse número inteiro de n. Veja um exemplo abaixo
+
+// let numero = 16
+let n = 16
+let i = 1
+
+while (i <= n) {
+     console.log (i)
+    i = i + 2
+} 
