fd05bb54d6dce5d641ef2d1bee710afeedf5ef11
Carolina Miranda <carolinamiranda.adm@gmail.com>
2022-08-29 16:01:29 -0300

    Pratica opcional, exibir se é numero primo ou não

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,24 @@
+//Números primos são aqueles que são divisíveis apenas pelo número 1 e por ele mesmo. Os números primos cumprem um papel essencial na área da criptografia, pois é por meio deles que conseguimos hoje criptografar informações.
+//Agora é com você! Crie um código no arquivo pratica-4.js que começando com a entrada de um número inteiro maior que zero, exiba Sim, o número ${numero} é primo! se este número for primo e Não, o número ${numero} não é primo! se ele não for um número primo. Como sugestão, pode colocar o nome da variável que vai armazenar esse valor de entrada de numero.
+
+
+//
+
+i = 2 // resolvendo operador
+
+let numero = 1500450271
+while (numero % i != 0)
+    i++  
+    
+if (numero == i) 
+    console.log ("Sim, o número 1500450271 é primo")
+else 
+    console.log ("Não, o número 1500450271 não é primo")
+
+
+    
+        
+
+    
+
+
